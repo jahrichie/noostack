e2b1333289cecdc77905aed162f2c8a6bd13be97
Richard OReilly <richardjosephoreilly@gmail.com>
2013-05-11 18:02:01 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Noo::Application.config.secret_token = 'b018c09286420256df9976cb59c067baa94835d4202345faaa41809c1a370c4cb40e5fc9949b8f003df0879cb4ab2353766535b8a3f2fa0fde4a65e61fc99ee9'
