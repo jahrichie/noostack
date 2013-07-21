e0c638d3cc47468a4321eb0d6021d720fe7a6f7b
jahrichie <roreilly@charitybuzz.com>
2013-07-20 20:38:40 -0400

    added auto complete technology

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,8 +3,8 @@ class Ingredient < ActiveRecord::Base
   #relationships
   belongs_to :stack
 
-  STUDIES = ["Coffee","Tea", "THC"]
+  BASE = ["Coffee","Tea", "THC"]
   attr_accessible :description, :name, :stack_id
   
-  validates_presence_of :name,:stack_id
+  # validates_presence_of :name,:stack_id
 end
